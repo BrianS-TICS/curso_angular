9ea4bb0709a9d0360c6bf27a0264e83131c63a1f
BrianS-TICS <briansanchezaguilar@gmail.com>
2022-10-14 22:49:46 -0600

    add notas y estilos

--- piano/src/app/app.component.ts
+++ piano/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'piano';
+
+  aplicarSonido(numTecla : number) : void{
+    const audio = new Audio();
+    audio.src = `../assets/sonidos/note${numTecla}.wav`;
+    audio.load();
+    audio.play();
+  }
 }
