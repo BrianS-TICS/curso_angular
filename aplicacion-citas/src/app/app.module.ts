1257d527382509ebd72880987ce196f8edbcd46d
BrianS-TICS <briansanchezaguilar@gmail.com>
2022-10-16 12:58:18 -0600

    add inicio de citas

---
+++ aplicacion-citas/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListCitasComponent } from './components/list-citas/list-citas.component';
+import { CrearCitasComponent } from './components/crear-citas/crear-citas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListCitasComponent,
+    CrearCitasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ aplicacion-citas/src/app/components/list-citas/list-citas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-citas',
+  templateUrl: './list-citas.component.html',
+  styleUrls: ['./list-citas.component.scss']
+})
+export class ListCitasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
