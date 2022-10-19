4a98da5f93fdadd0218358c0e34118ddb0bab75f
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2022-10-19 10:39:50 -0600

    add proyecto shortLink

---
+++ shortlink/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ShortUrlComponent } from './components/short-url/short-url.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ShortUrlComponent,
+    SpinnerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ shortlink/src/app/components/short-url/short-url.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-short-url',
+  templateUrl: './short-url.component.html',
+  styleUrls: ['./short-url.component.scss']
+})
+export class ShortUrlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
