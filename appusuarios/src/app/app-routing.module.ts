77bde6ae26938e7e8e08435bafde781420f316ef
BrianS-TICS <briansanchezaguilar@gmail.com>
2022-10-28 23:45:07 -0600

    Se agrega ruteo y diseño a componentes

--- appusuarios/src/app/app-routing.module.ts
+++ appusuarios/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', redirectTo: 'layout', pathMatch: 'full'},
+  { path: 'layout', component: LayoutComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path : '**', redirectTo: 'layout', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
