7db47ad0257c1277abff969248d3658910b6a977
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2022-11-01 22:31:59 -0700

    Se agrega edicion de empleados

--- abmEmpleados/src/app/services/empleado.service.ts
+++ abmEmpleados/src/app/services/empleado.service.ts
@@ -23,11 +23,24 @@ export class EmpleadoService {
     return this.listaEmpleados.slice();
   }
 
+  editEmpleado(empleado : Empleado, index : number){
+    this.listaEmpleados[index].nombreCompleto = empleado.nombreCompleto;
+    this.listaEmpleados[index].correo = empleado.correo;
+    this.listaEmpleados[index].fechaIngreso = empleado.fechaIngreso;
+    this.listaEmpleados[index].telefono = empleado.telefono;
+    this.listaEmpleados[index].estadoCivil = empleado.estadoCivil;
+    this.listaEmpleados[index].sexo = empleado.sexo;
+  }
+
   eliminarEmpleado(index : number){
     this.listaEmpleados.splice(index, 1);
   }
 
   agregarEmpleado(empleado : Empleado){
     this.listaEmpleados.unshift(empleado);
+  }
+
+  getEmpleado(index : number): Empleado{
+    return this.listaEmpleados[index];
   }
 }
